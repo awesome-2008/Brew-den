d4328c901c147777029b5aa92129bb35477fdc6a
Awesome Irechukwu <coola6263@gmail.com>
2026-09-14 11:47:11 +0100

    Add CORS headers

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const https = require('https');
 
 const app = express();
 app.use(express.json());
+
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname)));
 
 // Email transporter setup
